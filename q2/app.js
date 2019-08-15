e5a1f6c5f5bd7a86e3342868872a766d6f1cef8f
anithapaul1234 <anithapaulspyro@gmail.com>
2019-08-15 20:37:18 +0530

    secondhw

--- q2/app.js
+++ q2/app.js
@@ -1,5 +1,48 @@
 const Express=require('express');
 var app=new Express();
+
+books=[
+    {
+        'title':'Aatujeevitham',
+        'author':'Aatujeevitham',
+        'publisher':'Green Books',
+        'date_of_publication':'15 July 2008',
+        'distributor':'Green Books',
+        'price':199,
+        
+
+    },
+    {
+        title:" Khasakkinte Ithihasam ",
+        author:"O.V. Vijayan",
+        publisher:"Current Books",
+        date_of_publication:"28 January 1968",
+        distributor:"Current Books",
+        price:159,
+        
+
+    },
+    {
+        title:"Aalahayude Penmakal",
+        author:"Sara Joseph",
+        publisher:"Current Books",
+        date_of_publication:"1999",
+        distributor:"Current Books",
+        price:225,
+       
+    },
+    {
+        title:"Pathummayude Aadu",
+        author:"Vaikom Muhammad Basheer",
+        publisher:"DC Books",
+        date_of_publication:"April 1959",
+        distributor:"DC Books",
+        price:200,
+        
+
+    }];
+
+
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
